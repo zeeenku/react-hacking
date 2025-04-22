37372e1354bbb2482b4822ddf9ba84008443bc4d
zeeenku <enajjachi.zakariaa@gmail.com>
2025-04-22 15:18:44 +0200

    prototype added

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import CountProvider, { useCountStore } from './CountProvider';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const initialCount = 2;
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <CountProvider initialCount={initialCount}>
+    <StaticComponent/>
+    <DynamicComponent/>
+  </CountProvider>
   )
 }
 
-export default App
+function StaticComponent(){
+  return (<h1>Hello there! this is a static title to check if it gets rendered when changing the non used state here</h1>)
+}
+
+function DynamicComponent(){
+  const count = useCountStore((state)=>state.count);
+  const inc = useCountStore(state=>state.inc)
+  return (<>
+    <p>
+      Count is: {count};
+    </p>
+    <button onClick={inc}>Inc counter</button>
+  </>
+  
+);
+}
+
+export default App;
